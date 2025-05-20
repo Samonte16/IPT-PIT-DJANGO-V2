e7aa3ee6859e083aa10b82daea7e9fc09419c902
Samonte_CarlosAntonio <carlossamonte1@gmail.com>
2025-05-20 20:44:09 +0700

    TRY!

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,13 +6,13 @@ from .views import (
 )
 
 urlpatterns = [
-    path('signup/', signup_view, name='signup'),
-    path('signin/', signin_view, name='signin'),
-    path('verify-email/<uuid:token>/', verify_email, name='verify_email'),
-    path('admin-verify-email/<uuid:token>/', admin_verify_email, name='admin_verify_email'),
-    path('admin/register/', admin_register_view, name='admin_register'),
-    path('admin/login/', admin_login_view, name='admin_login'),
-    path('admin/verified-users/', verified_users_view, name='verified_users'),
-    path('forgot-password/', forgot_password_view, name='forgot_password'),
-    path('admin-forgot-password/', admin_forgot_password_view, name='admin_forgot_password'),
+    path('api/signup/', signup_view, name='signup'),
+    path('api/signin/', signin_view, name='signin'),
+    path('api/verify-email/<uuid:token>/', verify_email, name='verify_email'),
+    path('api/admin-verify-email/<uuid:token>/', admin_verify_email, name='admin_verify_email'),
+    path('api/admin/register/', admin_register_view, name='admin_register'),
+    path('api/admin/login/', admin_login_view, name='admin_login'),
+    path('api/admin/verified-users/', verified_users_view, name='verified_users'),
+    path('api/forgot-password/', forgot_password_view, name='forgot_password'),
+    path('api/admin-forgot-password/', admin_forgot_password_view, name='admin_forgot_password'),
 ]
