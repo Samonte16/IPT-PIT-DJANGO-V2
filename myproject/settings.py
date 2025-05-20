04fc2f89b96bf315d73e84eda251108f22bb8382
Samonte_CarlosAntonio <carlossamonte1@gmail.com>
2025-05-20 22:09:59 +0700

    5

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l%7=vew73dn9zbbb@k=&3ag=(_8tb7l*3v^_7m3@fy(&mpqf=2'
+SECRET_KEY = os.getenv('django-insecure-l%7=vew73dn9zbbb@k=&3ag=(_8tb7l*3v^_7m3@fy(&mpqf=2')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['ipt-pit-django-v2.onrender.com']
+ALLOWED_HOSTS = ['https://ipt-pit-django-v2.onrender.com']
 
 
 # Application definition
